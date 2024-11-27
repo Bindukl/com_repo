841e76423cab61853e35e143e17d2ad2c9c157df
Bindu <bindulokesh9@gmail.com>
2024-11-27 19:06:28 +0530

    first commit

---
+++ NewProject_Java_Repo/src/test/java/newscriptTest/JavaTest.java
@@ -0,0 +1,21 @@
+package newscriptTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaTest {
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+		driver.findElement(By.id("email")).sendKeys("bindulokesh9@gmail.com");
+		Thread.sleep(5000);
+		driver.findElement(By.id("pass")).sendKeys("bindulokesh9@gmail.com");
+	}
+
+}
